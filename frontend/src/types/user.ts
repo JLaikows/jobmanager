f0d6986b5f48d6b044344c3fe6f2945c977974ff
JLaikows <joshua@laikowski.com>
2023-12-05 14:40:03 -0500

    Moved user Actions

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -5,9 +5,3 @@ export type TUser = {
   github?: string;
   linkedIn: string;
 };
-
-export enum UserActions {
-  LOGIN = 'LOGIN',
-  LOGOUT = 'LOGOUT',
-  UPDATE_PREFERENCES = 'UPDATE_PREFERENCES',
-}
