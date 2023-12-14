4a74757d896b0ee1a7c2655e9e3cd871271126ea
JLaikows <joshua@laikowski.com>
2023-12-14 18:01:38 -0500

    Organize opportunities by lastChecked

--- frontend/src/components/mobile/OpportunitiyTable/index.ts
+++ frontend/src/components/mobile/OpportunitiyTable/index.ts
@@ -3,7 +3,15 @@ import { getAllOpportunities } from '../../../actions/opportunities';
 import { MobileOpportunityTable } from './component';
 
 const mSTP = (state: any) => ({
-  opportunities: Object.values(state.entities.opportunities),
+  opportunities: Object.values(state.entities.opportunities).sort(
+    (a: any, b: any) => {
+      //sorts the values by the lastChecked, from oldest to most recent
+      return (
+        (new Date(a.lastChecked) as unknown as number) -
+        (new Date(b.lastChecked) as unknown as number)
+      );
+    },
+  ),
 });
 
 const mDTP = (dispatch: any) => ({
